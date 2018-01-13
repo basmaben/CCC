77d733d2d21fa806c9ede4b9179a14dbf60429ff
basma <benyoussef.basma95@gmail.com>
2018-01-13 10:56:20 +0000

    commit 2

--- src/ProblemA/scorify.java
+++ src/ProblemA/scorify.java
@@ -1,9 +1,23 @@
 package ProblemA;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class scorify {
 
-    Scanner in=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String s1,s2 ,t[]=new String[2] ;
+            s1=in.next();
+            s2=in.next();
+           // System.out.println(t.toString());
+            t=s1.split(" ");
+
+            System.out.println("Bienvenue "+s1+" "+s2+" au CCC.");
+        }
+
+    }
 
 }
--- src/ProblemB/scorify.java
+++ src/ProblemB/scorify.java
@@ -1,5 +1,25 @@
 package ProblemB;
 
+import java.util.Scanner;
+
 public class scorify {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            int a,b,c;
+            a=in.nextInt();
+            b=in.nextInt();
+            c=in.nextInt();
+
+            if(a+b>c && b+c>a && a+c>b)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+
+    }
 }
